9080600d0e665b5b4538285c895ce35703fbc659
abdulaziz_ulashbaev <abdulaziz_ulashbaev@student.itpu.uz>
2024-01-15 17:53:58 +0500

    StudentRepo done singleton and added loadData() and uploadData() functions

--- src/db/StudentRepo.java
+++ src/db/StudentRepo.java
@@ -2,9 +2,56 @@ package db;
 
 import entity.Student;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentRepo implements Repository<Student>{
+
+    private static String PATH = "src/db/student_db.txt";
+    private List<Student> students;
+    private static StudentRepo singleton;
+
+    public StudentRepo(List<Student> students) {
+        this.students = students;
+    }
+
+    public static StudentRepo of(){
+        if(singleton==null){
+            singleton = new StudentRepo(loadData());
+        }
+        return singleton;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static List<Student> loadData() {
+
+        try (InputStream is = new FileInputStream(PATH);
+             ObjectInputStream objectInputStream = new ObjectInputStream(is);
+
+        ){
+            return (List<Student>) objectInputStream.readObject();
+        } catch (EOFException e){
+            return new ArrayList<>();
+        } catch(IOException | ClassNotFoundException e) {
+            throw new RuntimeException();
+        }
+
+    }
+
+    private void uploadData() {
+
+        try (
+                OutputStream os = new FileOutputStream(PATH);
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(os);
+        ){
+            objectOutputStream.writeObject(students);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
     @Override
     public void create(Student student) {
 
